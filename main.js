8c69bfde42744ce448c8a4f5993f1327f7699bb0
Sarvuu <76529861+sarvesh2010@users.noreply.github.com>
2022-01-28 20:43:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log('poseNet is intialized');
+}
+
+function draw() {
+    background('#00FFFF');
+}
